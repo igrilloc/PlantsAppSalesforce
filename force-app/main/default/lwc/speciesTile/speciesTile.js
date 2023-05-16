d7ca27662e11d2be53f5b570177bacce06f2ca7b
Ignacio Grillo Caimary <igrilloc@pop-os.localdomain>
2023-05-16 20:18:09 -0300

    Import test data & create first LWC speciesTile

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+
+
+export default class SpeciesTile extends LightningElement {
+
+    specie = {
+        Name: "Jazmin",
+        Description: "Olorosa y bonita planta trepadora",
+        Image_URL__c: "https://i.pinimg.com/originals/88/a4/9f/88a49f73cb34bb49ea799087ad2fba15.jpg",
+        Location__c: "Indoors,Outdoors"
+    };
+
+}
