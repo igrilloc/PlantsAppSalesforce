15ee8c9ad7b0dc46475499c896c8cf5a78adea90
Ignacio Grillo Caimary <igrilloc@pop-os.localdomain>
2023-05-18 02:46:55 -0300

    Create SpeciesService Apex class. Translation workbench template for:each, callout to Apex class

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,14 +1,45 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
 
+export default class SpeciesTile extends NavigationMixin(LightningElement) {
 
-export default class SpeciesTile extends LightningElement {
-
+    /*
     specie = {
         Name: "Jazmin",
-        Description: "Olorosa y bonita planta trepadora",
+        Description__c: "Olorosa y bonita planta trepadora",
         Image_URL__c: "https://i.pinimg.com/originals/88/a4/9f/88a49f73cb34bb49ea799087ad2fba15.jpg",
-        Location__c: "Indoors,Outdoors"
+        Location__c: "Indoors, Outdoors"
     };
+    */
+
+    @api specie;
+
+
+    // specie.Location__c = "Indoors"
+    // specie.Location__c = "Outdoors"
+    // specie.Location__c = "Indoors;Outdoors"
+
+    get isOutdoor() {
+        return this.specie.Location__c.includes("Outdoors");
+    }
+
+    get isIndoor() {
+        return this.specie.Location__c.includes("Indoors");
+    }
+
+    navigateToRecordViewPage() {
+        // View a custom object record
+        this[NavigationMixin.Navigate](
+            {
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: this.specie.Id,
+                    objectApiName: 'Species__c', // objectApiName is optional
+                    actionName: 'view'
+                }
+            }
+        );
+    }
 
 }
